bff16a75ea906a6870b3c1950639e3a092781458
ahb647 <58379725+ahb647@users.noreply.github.com>
2025-10-01 21:03:26 -0400

    Se creo un app para evitar un crasheo

---
+++ app/src/main/java/edu/ucne/adrian_hernandez_bonilla_AP2_p1/RegistroEntradaHuacalesApp.kt
@@ -0,0 +1,7 @@
+package edu.ucne.adrian_hernandez_bonilla_AP2_p1
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RegistroEntradaHuacalesApp : Application()
