e6a3ac65ff68d03464027a0d374800d3121be889
ahb647 <58379725+ahb647@users.noreply.github.com>
2025-10-01 21:06:24 -0400

    Se creo el archivo route para las rutas

---
+++ app/src/main/java/edu/ucne/adrian_hernandez_bonilla_AP2_p1/ui/theme/Util/Route.kt
@@ -0,0 +1,11 @@
+package edu.ucne.adrian_hernandez_bonilla_AP2_p1.ui.theme.Util
+
+import edu.ucne.adrian_hernandez_bonilla_AP2_p1.Domain.Model.EntradaHuacales
+
+object Route {
+
+    const val ENTRADAHUACALES_LIST = "EntradaHuacales_list"
+    const val ENTRADAHUACALES_FORM = "EntradaHuacales_form"
+
+
+}
